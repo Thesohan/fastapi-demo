4900d7431b502dc6ec5cc1da8a3cab6af4616951
Thesohan <sohan.cse16@nituk.ac.in>
2023-07-15 17:34:52 +0530

    handson path paramenters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -11,3 +12,34 @@ async def root():
 @app.get("/normal-root")
 def normal_root():
     return {"message": "Hello world!"}
+
+
+"""
+ order matters, items/test should be bfore items/{item_id}
+"""
+
+
+@app.get("/items/test")
+async def read_item2():
+    return {"item2": "test2"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    return {"item_id": item_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}

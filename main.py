c9ea19c5a6b96951667664660ed40f7a32384d4d
Thesohan <sohan.cse16@nituk.ac.in>
2023-07-18 00:46:23 +0530

    dependencies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from enum import Enum
+from typing import Annotated
 
 app = FastAPI()
 
@@ -43,3 +44,43 @@ async def get_model(model_name: ModelName):
     if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+# Query parameters
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def read_item_new(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description":
+             """This is an amazing item that has a long description"""}
+        )
+    return item
+
+
+async def common_parameters(q: str | None = None, skip: int = 0,
+                            limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/dependencies/items")
+async def read_users_dependencies(commons: Annotated[dict, Depends
+                                                     (common_parameters)]):
+    return
